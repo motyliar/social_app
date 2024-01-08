4b5d03edd4afc5c6ec0252d92263c246cedf3333
motyliar <j.turostowski@gmail.com>
2024-01-08 15:42:53 +0100

    add respository helpers

---
+++ lib/core/datahelpers/repository_helpers/http_get_data_handler.dart
@@ -0,0 +1,14 @@
+import 'package:climbapp/core/datahelpers/repository_helpers/http_repository_handler.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+
+class HttpGetDataHandler extends HttpRepositoryHandler {
+  HttpGetDataHandler({required super.params});
+
+  final http.Client _client = http.Client();
+
+  @override
+  Future<Response> _getResponseFromServer() async {
+    return await _client.get((params.url!), headers: params.headers);
+  }
+}
---
+++ lib/core/datahelpers/repository_helpers/http_repository_handler.dart
@@ -0,0 +1,25 @@
+import 'package:climbapp/core/datahelpers/handlers/response_handlers.impl.dart';
+import 'package:climbapp/core/datahelpers/params/repository_params.dart';
+import 'package:climbapp/core/utils/utils.dart';
+import 'package:http/http.dart';
+
+abstract class HttpRepositoryHandler {
+  const HttpRepositoryHandler({required this.params});
+  final RepositoryParams params;
+
+  Future<Response> getResponseFromServer();
+
+  Future<Response> sendRequest() async {
+    return await getResponseFromServer();
+  }
+
+  EitherFunc<T> returnData<T>() async {
+    final response = await sendRequest();
+    return _handleResponse<T>(response);
+  }
+
+  EitherFunc<T> _handleResponse<T>(Response response) async {
+    final handler = ResponseHandler();
+    return handler.checkStatusCodeAndReturnData(response);
+  }
+}
