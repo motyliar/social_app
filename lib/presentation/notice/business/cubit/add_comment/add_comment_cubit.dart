494c475718ae0da59347625f52bcd2c80f1258e2
motyliar <j.turostowski@gmail.com>
2024-01-15 13:03:46 +0100

    create cubit to service add comment usecase

--- lib/presentation/notice/business/cubit/add_comment/add_comment_cubit.dart
+++ lib/presentation/notice/business/cubit/add_comment/add_comment_cubit.dart
@@ -1,8 +1,36 @@
 import 'package:bloc/bloc.dart';
+import 'package:climbapp/core/datahelpers/params/notice/notice_params.dart';
+import 'package:climbapp/core/datahelpers/status_service/response_status.dart';
+import 'package:climbapp/core/error/exceptions/exceptions.dart';
+import 'package:climbapp/domains/notice/usecases/create_notice_comment_usecase.dart';
 import 'package:equatable/equatable.dart';
 
 part 'add_comment_state.dart';
 
 class AddCommentCubit extends Cubit<AddCommentState> {
-  AddCommentCubit() : super(AddCommentInitial());
+  final CreateNoticeCommentUseCase _comment;
+  AddCommentCubit({required CreateNoticeCommentUseCase comment})
+      : _comment = comment,
+        super(AddCommentState());
+
+  Future<void> addNewComment(CreateNoticeCommentsParams params) async {
+    await _addNewComment(params);
+  }
+
+  Future<void> _addNewComment(CreateNoticeCommentsParams params) async {
+    try {
+      final response = await _getResponseFromDB(params);
+      emit(AddCommentSuccess(status: response));
+    } on ServerException catch (e) {
+      emit(AddCommentFailed(message: e.message));
+    }
+  }
+
+  Future<ResponseStatus> _getResponseFromDB(
+      CreateNoticeCommentsParams params) async {
+    final response = await _comment.execute(params).then((response) => response
+        .fold((f) => throw ServerException(f.toString()), (data) => data));
+
+    return response;
+  }
 }
