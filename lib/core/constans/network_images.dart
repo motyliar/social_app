b0f3c25e415c1c8730e492676053f9c66f024018
motyliar <j.turostowski@gmail.com>
2024-03-13 11:55:06 +0100

    redirecting images URL

--- lib/core/constans/network_images.dart
+++ lib/core/constans/network_images.dart
@@ -1,25 +1,27 @@
 class ImagesURL {
   static const signInTopBackground =
-      'http://motyliar.webd.pro/social/login_background.jpg';
+      'http://65.21.202.169:20119/app_images/login_background.jpg';
 
   static const signInBottomBackground =
-      'http://motyliar.webd.pro/social/bottom_background.jpg';
+      'http://65.21.202.169:20119/app_images/bottom_background.jpg';
 
   static const warningSnackBarBackground =
-      'http://motyliar.webd.pro/social/annoucment.png';
+      'http://65.21.202.169:20119/app_images/annoucment.png';
 
   static const successSnackBarBackground =
-      'http://motyliar.webd.pro/social/approve.png';
+      'http://65.21.202.169:20119/app_images/approve.png';
 
-  static const appLogo = 'http://motyliar.webd.pro/social/appLogo.png';
+  static const appLogo = 'http://65.21.202.169:20119/app_images/appLogo.png';
   static const appLogoButton =
-      'http://motyliar.webd.pro/social/appLogo_button.png';
+      'http://65.21.202.169:20119/app_images/appLogo_button.png';
   static const appLogoSmall =
-      'http://motyliar.webd.pro/social/appLogo_small.png';
+      'http://65.21.202.169:20119/app_images/appLogo_small.png';
   static const appLogoSmallM =
-      'http://motyliar.webd.pro/social/appLogo_small_m.png';
-  static const messageIcon = 'http://motyliar.webd.pro/social/message.png';
-  static const friendsIcon = 'http://motyliar.webd.pro/social/friendslogo.png';
-  static const waveIcon = 'http://motyliar.webd.pro/social/wave.png';
-  static const approve = 'http://motyliar.webd.pro/social/approve.png';
+      'http://65.21.202.169:20119/app_images/appLogo_small_m.png';
+  static const messageIcon =
+      'http://65.21.202.169:20119/app_images/message.png';
+  static const friendsIcon =
+      'http://65.21.202.169:20119/app_images/friendslogo.png';
+  static const waveIcon = 'http://65.21.202.169:20119/app_images/wave.png';
+  static const approve = 'http://65.21.202.169:20119/app_images/approve.png';
 }
