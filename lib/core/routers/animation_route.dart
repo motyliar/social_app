f253c0c219b981f8382d96df36df076a2349abb5
motyliar <j.turostowski@gmail.com>
2024-03-22 11:43:55 +0100

    create animation route class

---
+++ lib/core/routers/animation_route.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class AnimationRoute {
+  static Route slideUpRoute(
+      {required Widget direction, required String route}) {
+    return PageRouteBuilder(
+      settings: RouteSettings(name: route),
+      pageBuilder: ((context, animation, secondaryAnimation) => direction),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = const Offset(0.0, 1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
+  static Route slideDownRoute(
+      {required Widget direction, required String route}) {
+    return PageRouteBuilder(
+      settings: RouteSettings(name: route),
+      pageBuilder: ((context, animation, secondaryAnimation) => direction),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = const Offset(0.0, -1.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+
+  static Route rightToLeft({required Widget direction, required String route}) {
+    return PageRouteBuilder(
+      settings: RouteSettings(name: route),
+      pageBuilder: ((context, animation, secondaryAnimation) => direction),
+      transitionsBuilder: (context, animation, secondaryAnimation, child) {
+        var begin = const Offset(1.0, 0.0);
+        var end = Offset.zero;
+        var curve = Curves.ease;
+
+        var tween =
+            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
+
+        return SlideTransition(
+          position: animation.drive(tween),
+          child: child,
+        );
+      },
+    );
+  }
+}
