204833aa9a479ed92fdb79d9daefe43e070cfff3
motyliar <j.turostowski@gmail.com>
2024-03-26 12:01:07 +0100

    create notify repo impl with send function

---
+++ lib/data/notification/repository/notify_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'package:climbapp/core/datahelpers/params/notification/notification.dart';
+import 'package:climbapp/core/error/exceptions/exceptions.dart';
+import 'package:climbapp/core/utils/utils.dart';
+import 'package:climbapp/data/notification/datasources/remote/notify_remote_data_source.dart';
+import 'package:climbapp/domains/notification/repository/notify_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class NotifyRepositoryImpl extends NotifyRepository {
+  NotifyRepositoryImpl({required INotifyRemoteDataSource remote})
+      : _remote = remote;
+  final INotifyRemoteDataSource _remote;
+
+  @override
+  EitherFunc<void> sendNotify(NotificationParams params) async {
+    try {
+      return await Utils().performNetworkOperation(
+        () async => await _remote.addNewNotify(params).then(
+              (value) => Right(
+                value,
+              ),
+            ),
+      );
+    } on ServerException catch (e) {
+      return Left(e);
+    } catch (e) {
+      return Left(ServerException.message(e.toString()));
+    }
+  }
+}
