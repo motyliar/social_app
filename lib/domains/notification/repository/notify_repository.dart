6bd308d0c4863ce2813771cbb15d44432280255e
motyliar <j.turostowski@gmail.com>
2024-03-26 15:22:42 +0100

    create domain layer for fetch user notify

--- lib/domains/notification/repository/notify_repository.dart
+++ lib/domains/notification/repository/notify_repository.dart
@@ -1,7 +1,10 @@
 // ignore: unused_import
 import 'package:climbapp/core/datahelpers/params/notification/notification.dart';
 import 'package:climbapp/core/utils/utils.dart';
+import 'package:climbapp/domains/notification/entities/notification_entity.dart';
 
 abstract class NotifyRepository {
   EitherFunc<void> sendNotify(NotificationParams params);
+  EitherFunc<List<NotificationEntity>> fetchUserNotify(
+      NotificationParams params);
 }
